e5771f3e94cc29235ead5d4592490cc51aad5eca
Nur E Allhi <noorefty1@gmail.com>
2025-08-13 08:28:19 +0000

    By default action will be hide,

--- src/components/bank-tab.tsx
+++ src/components/bank-tab.tsx
@@ -44,7 +44,7 @@ export function BankTab() {
   const [currentMonth, setCurrentMonth] = useState(new Date());
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage, setItemsPerPage] = useState(10);
-  const [showActions, setShowActions] = useState(true);
+  const [showActions, setShowActions] = useState(false);
   const isMobile = useIsMobile();
 
   const filteredByMonth = useMemo(() => {
