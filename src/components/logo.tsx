1b2c27a17c3e9af5404a85058d5109a84c04cbad
Nur E Allhi <noorefty1@gmail.com>
2025-08-15 18:13:54 +0000

    not showing

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,18 +1,12 @@
-import Image from 'next/image';
 import { cn } from '@/lib/utils';
 
 const Logo = ({ className }: { className?: string }) => {
   return (
-    <div className={cn('relative', className)}>
-      <Image
-        src="/logo.png"
-        alt="ShipShape Ledger Logo"
-        fill
-        sizes="100vw"
-        style={{ objectFit: 'contain' }}
-        priority 
-      />
-    </div>
+    <img
+      src="/logo.png"
+      alt="ShipShape Ledger Logo"
+      className={cn(className)}
+    />
   );
 };
 
