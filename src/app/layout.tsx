4b3c6bf55b990618f1fb9d5d2dc417fa43b1bae6
Nur E Allhi <noorefty1@gmail.com>
2025-08-16 14:33:33 +0000

    App loading is taking huge time, How to manage them?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,30 +10,6 @@ export const metadata: Metadata = {
   description: 'Manage your inventory, sales, and finances for Ha-Mim Iron Mart.',
 };
 
-const fontUrls = [
-  "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap",
-  "https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap",
-  "https://fonts.googleapis.com/css2?family=Merriweather:wght@400;700&display=swap",
-  "https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700&display=swap",
-  "https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700&display=swap",
-  "https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;500;600;700&display=swap",
-  "https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap",
-  "https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;500&display=swap",
-  "https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;500&display=swap",
-  "https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@400;500&display=swap",
-];
-
-
-function FontLoader() {
-  return (
-    <>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-      {fontUrls.map(url => <link key={url} href={url} rel="stylesheet" />)}
-    </>
-  )
-}
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -42,7 +18,8 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <FontLoader />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </head>
       <body>
         <AppProvider>
