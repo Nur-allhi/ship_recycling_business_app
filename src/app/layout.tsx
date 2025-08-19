17267f4923e23079e274cd537712e4a1e8e4de5a
Nur E Allhi <noorefty1@gmail.com>
2025-08-19 17:58:35 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 
 import type { Metadata } from 'next';
 import './globals.css';
-import { Toaster } from 'sonner';
+import { Toaster } from '@/components/ui/toaster';
 import { AppProvider } from '@/app/store';
 import { LayoutProvider } from '@/components/layout-provider';
 
