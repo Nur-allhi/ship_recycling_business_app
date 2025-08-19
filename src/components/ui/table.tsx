ad4f3ee0e71d06d1af90b9b42fd8e61d876724e8
Nur E Allhi <noorefty1@gmail.com>
2025-08-19 17:03:19 +0000

    Make border to every table lets test

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -7,11 +7,13 @@ const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
 >(({ className, ...props }, ref) => (
-  <table
-    ref={ref}
-    className={cn("w-full caption-bottom text-sm", className)}
-    {...props}
-  />
+  <div className="relative w-full overflow-auto">
+    <table
+      ref={ref}
+      className={cn("w-full caption-bottom text-sm", className)}
+      {...props}
+    />
+  </div>
 ))
 Table.displayName = "Table"
 
@@ -72,7 +74,7 @@ const TableHead = React.forwardRef<
   <th
     ref={ref}
     className={cn(
-      "h-12 px-2 align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0 md:px-4 [&:has(button)]:p-0",
+      "h-12 px-2 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
       className
     )}
     {...props}
@@ -86,7 +88,7 @@ const TableCell = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <td
     ref={ref}
-    className={cn("p-2 align-middle [&:has([role=checkbox])]:pr-0 md:p-4", className)}
+    className={cn("p-2 align-middle [&:has([role=checkbox])]:pr-0", className)}
     {...props}
   />
 ))
@@ -114,4 +116,3 @@ export {
   TableCell,
   TableCaption,
 }
-    
