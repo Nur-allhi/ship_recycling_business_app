ce88c1754b04a993d51fc3d70ed7b4b52d199afc
Nur E Allhi <noorefty1@gmail.com>
2025-08-19 17:59:54 +0000

    Error: Objects are not valid as a React child (found: object with keys {

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -2,18 +2,12 @@
 "use client"
 
 // Inspired by react-hot-toast library
-import { toast as sonnerToast, Toaster as SonnerToaster } from "sonner";
+import { toast } from "sonner";
 
 const useToast = () => {
   return {
-    toast: sonnerToast,
+    toast,
   }
 }
 
-// Re-exporting the Toaster component from sonner
-const Toaster = SonnerToaster;
-
-// Re-exporting the toast function for convenience
-const toast = sonnerToast;
-
-export { useToast, Toaster, toast };
+export { useToast, toast };
