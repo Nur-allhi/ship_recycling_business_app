cee80f78bae0f380d5af894f2395adf2e37fa493
Nur E Allhi <noorefty1@gmail.com>
2025-09-05 07:12:19 +0000

    see there is nothing apear

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,6 @@ import LogoutOverlayWrapper from '@/components/logout-overlay-wrapper'; // Impor
 export const metadata: Metadata = {
   title: 'Ha-Mim Iron Mart',
   description: 'Manage your inventory, sales, and finances for Ha-Mim Iron Mart.',
-  manifest: '/manifest.json',
   themeColor: '#ffffff',
   icons: {
     apple: '/icon-192x192.png',
@@ -24,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="manifest" href="/manifest.json" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;700&family=Roboto+Mono:wght@400;700&display=swap" rel="stylesheet" />
